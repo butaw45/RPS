09bbdb886a52e17a9bbe67c29ecc7813be0cf5d8
haris <kumantheas@gmail.com>
2022-07-23 09:37:12 +0700

    End

--- src/App.vue
+++ src/App.vue
@@ -62,9 +62,9 @@ const SaveGame = () => {
 };
 
 const LoadGame = () => {
-  wins.value = localStorage.getItem('wins');
-  draws.value = localStorage.getItem('draws');
-  losses.value = localStorage.getItem('losses');
+  wins.value = parseInt(localStorage.getItem('wins')) || 0;
+  draws.value = parseInt(localStorage.getItem('draws')) || 0;
+  losses.value = parseInt(localStorage.getItem('losses')) || 0;
 };
 
 const ResetRound = () => {
@@ -84,4 +84,63 @@ onMounted(() => {
 });
 </script>
 
-<template><h1>Hello, World!</h1></template>
+<template>
+  <div class="bg-gray-700 text-white text-center min-h-screen flex flex-col">
+    <header class="container mx-auto p-6">
+      <h1 class="text-4xl font-bold">Rock, Paper, Scissors</h1>
+    </header>
+
+    <main class="container mx-auto p-6 flex-1">
+      <div
+        v-if="choice === null"
+        class="flex items-center justify-center -mx-6"
+      >
+        <button
+          @click="play('rock')"
+          class="bg-white rounded-full shadow-lg w-64 p-12 mx-6 transition-colors duration-300 hover:bg-pink-500"
+        >
+          <img src="./assets/RockIcon.svg" alt="Rock" class="w-full" />
+        </button>
+
+        <button
+          @click="play('paper')"
+          class="bg-white rounded-full shadow-lg w-64 p-12 mx-6 transition-colors duration-300 hover:bg-green-500"
+        >
+          <img src="./assets/PaperIcon.svg" alt="Paper" class="w-full" />
+        </button>
+
+        <button
+          @click="play('scissors')"
+          class="bg-white rounded-full shadow-lg w-64 p-12 mx-6 transition-colors duration-300 hover:bg-yellow-500"
+        >
+          <img src="./assets/ScissorsIcon.svg" alt="Scissors" class="w-full" />
+        </button>
+      </div>
+
+      <div v-else>
+        <div class="text-3xl mb-4">
+          You picked <span class="text-pink-500">{{ choice }}</span>
+        </div>
+
+        <div class="text-3xl mb-4">
+          The computed picked
+          <span class="text-green-500">{{ computerChoice }}</span>
+        </div>
+
+        <div class="text-6xl mb-12">
+          {{ verdict }}
+        </div>
+
+        <button @click="ResetRound" class="bg-pink-500 text-lg py-2 px-4">
+          Reset
+        </button>
+      </div>
+
+      <div class="mt-12 text-3xl mb-4">
+        {{ wins }} : {{ draws }} : {{ losses }}
+      </div>
+
+      <div class="text-lg">Win rate: {{ Math.round(winPercentage) }}</div>
+    </main>
+  </div>
+</template>
